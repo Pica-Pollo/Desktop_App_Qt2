d08f62bf3ba0872861dc64dad5cdd31481a5d8a8
Pica-Pollo <dayronearias@gmail.com>
2026-08-26 12:34:04 -0400

    Created Project
    This version work.

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QWidget window;
+
+    QVBoxLayout *layout = new QVBoxLayout(&window);
+
+    QLabel *label = new QLabel("Hola");
+    QPushButton *button = new QPushButton("Cambiar");
+
+    layout->addWidget(label);
+    layout->addWidget(button);
+
+    QObject::connect(button, &QPushButton::clicked,
+                     [label]()
+                     {
+                         label->setText("Hola Aruk");
+                     });
+
+    window.show();
+
+    return app.exec();
+}
